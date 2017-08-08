18cb310a921ee61bf4eb9717c298cbbf9e60ae04
Elrey Belmonti and Mitch Long <mitch@>
2017-08-08 11:08:59 -0700

    add jquery slector to add border around image when a click target is click

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
   $("button#light-theme").click(function(){
    $("body").removeClass();
+   $("img").removeClass();
   });
   $("button#dark-theme").click(function(){
     $("body").removeClass();
     $("body").addClass("dark-background");
+  });
+  $("#frame").click(function(){
+    $("img").removeClass();
+    $("img").addClass("dog-frame");
   });
 });
