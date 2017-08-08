5a063fb4d0482574f4fb9c10a7a4cea1b11e6d7e
Elrey Belmonti and Mitch Long <mitch@>
2017-08-08 10:49:01 -0700

    add dog img and add jQuery selector to change background to black and text to white.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  // $("button#light-theme").click(function(){
+  //
+  // });
+  $("button#dark-theme").click(function(){
+    $("body").addClass("dark-background");
+  });
+});
